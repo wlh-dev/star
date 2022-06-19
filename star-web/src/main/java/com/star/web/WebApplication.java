835be545724558964221d939618323539cc24b89
王录华 <825828826@qq.com>
2022-06-19 23:25:26 +0800

    add webApplication

---
+++ star-web/src/main/java/com/star/web/WebApplication.java
@@ -0,0 +1,15 @@
+package com.star.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : wlh
+ * @date Date : 2022年06月19日 23:08
+ */
+@SpringBootApplication
+public class WebApplication {
+		public static void main(String[] args) {
+				SpringApplication.run(WebApplication.class,args);
+		}
+}
