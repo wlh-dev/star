bb3c17efbe30a12eac10695cbadd61850a4498a2
王录华 <825828826@qq.com>
2022-06-20 09:29:03 +0800

    去掉自动装配MongoAutoConfiguration

--- star-web/src/main/java/com/star/web/WebApplication.java
+++ star-web/src/main/java/com/star/web/WebApplication.java
@@ -2,12 +2,13 @@ package com.star.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /**
  * @author : wlh
  * @date Date : 2022年06月19日 23:08
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class })
 public class WebApplication {
 		public static void main(String[] args) {
 				SpringApplication.run(WebApplication.class,args);
