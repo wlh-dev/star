ed3d1700f41d5b0e4c93193a426974dee2486fac
18577307017 <825828826@qq.com>
2023-08-13 12:31:32 +0800

    修改queue

--- star-web/src/main/java/com/star/wlh/config/demo/block/ArrayBlockingQueueTest.java
+++ star-web/src/main/java/com/star/wlh/config/demo/block/ArrayBlockingQueueTest.java
@@ -1,13 +1,14 @@
 package com.star.wlh.config.demo.block;
 
+import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class ArrayBlockingQueueTest {
     //一个由数组结构组成的有界阻塞队列
-    public static void main(String[] args) throws InterruptedException {
-        ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(1024);
-        for (int i = 0; i < 1025; i++) {
-            queue.put("queue\t"+i);
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayBlockingQueue<>(1024);
+        for (int i = 0; i < 1024; i++) {
+           queue.add("queue"+i);
         }
     }
 }
